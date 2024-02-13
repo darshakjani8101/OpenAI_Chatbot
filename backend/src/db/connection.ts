d1950721d688346b0856d0ab3cfe2e63e778eeac
darshakjani8101 <darshakjani8101@gmail.com>
2024-02-13 19:17:51 +0530

    Created basic express application and setup mongodb. Updated Readme file.

---
+++ backend/src/db/connection.ts
@@ -0,0 +1,21 @@
+import { connect, disconnect } from "mongoose"
+
+async function connectToDatabase() {
+    try {
+        await connect(process.env.MONGODB_URL)
+    } catch (error) {
+        console.log("Cannot connect to MongoDB !!!", error)
+        throw new Error("Cannot connect to MongoDB !!!")
+    }
+}
+
+async function disconnectFromDatabase() {
+    try {
+        await disconnect()
+    } catch (error) {
+        console.log("Cannot disconnect from MongoDB !!!", error)
+        throw new Error("Cannot disconnect from MongoDB !!!")
+    }
+}
+
+export { connectToDatabase, disconnectFromDatabase }
